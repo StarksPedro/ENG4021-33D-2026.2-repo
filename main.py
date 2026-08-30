ceb1eb43a68d6ca00cdb40cc05950f452597a6fe
Pedro Ferreira <pedro0108luiz@gmail.com>
2026-08-30 01:17:15 -0300

    tinha esquecido dos prints

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
 def escolhaDeOperação():
     while True:
-        operador= input("Escolha uma das operações:\n adição \n subtração \n multiplicação \n divisão\n exponenciação \n raidiciação\n divisão inteira\n porcentagem ou digite 0 para encerrar:").lower()
+        operador= input("Escolha uma das operações:\n adição \n subtração \n multiplicação \n divisão\n exponenciação \n raidiciação\n divisão inteira\n porcentagem\n ou\n digite 0 para encerrar:").lower()
+        if operador == "0":
+            print("Operação encerrada")
+            break
         n1= float(input("Digite o primeiro número:"))
         n2= float(input("Digite o segundo número:")) 
         if operador == "adição":
-            Adição = calcula_soma(n1,n2)
+            print("----------------")
+            print("\n")
+            print(calcula_soma(n1,n2))
+            print("\n")
+            print("----------------")
+            
         elif operador == "subtração":
-            Subtração = calcula_subtração(n1,n2)
+            print("----------------")
+            print("\n")
+            print(calcula_subtração(n1,n2))
+            print("\n")
+            print("----------------")
+            
         elif operador == "multiplicação":
-            Multiplicação = calcula_multiplicação(n1,n2)
+            print("----------------")
+            print("\n")
+            print(calcula_multiplicação(n1,n2))
+            print("\n")
+            print("----------------")
+            
         elif operador == "divisão":
-            Divisão = calcula_divisão(n1,n2)
+            print("----------------")
+            print("\n")
+            print(calcula_divisão(n1,n2))
+            print("\n")
+            print("----------------")
+            
         elif operador == "exponenciação":
-            Exponenciação = calcula_exponenciação(n1,n2)
+            print("----------------")
+            print("\n")
+            print(calcula_exponenciação(n1,n2))
+            print("\n")
+            print("----------------")
+            
         elif operador == "radiciação":
-            Radiciação = calcula_radiciação(n1,n2)
-        elif operador == "divisão inteira":
-            DivisãoInteira = calcula_divisão_inteira(n1,n2)
+            print("----------------")
+            print("\n")
+            print(calcula_radiciação(n1,n2))
+            print("\n")
+            print("----------------")
+            
+        elif operador == "divisao inteira":
+            print("----------------")
+            print("\n")
+            print(calcula_divisão_inteira(n1,n2))
+            print("\n")
+            print("----------------")
+            
         elif operador == "porcentagem":
-            Porcentagem = calcula_porcentagem(n1,n2)
-        elif operador == "0":
-            print("Operação encerrada")
-            break
+            print("----------------")
+            print("\n")
+            print(calcula_porcentagem(n1,n2))
+            print("\n")
+            print("----------------")
+          
 
 escolhaDeOperação()
