b3b41738bcd2f1ac58891e6c9cf990bdde2dfc23
Pedro Ferreira <pedro0108luiz@gmail.com>
2026-09-03 23:08:22 -0300

    Add modulo operation and fix input prompts

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def calcula_resto(n1,n2):
+    return n1 % n2
+
+def calcula_radiciação(n1, n2):
+     if n1 < 0 and (int(n2) % 2 == 1 or int(n2) % 2 == -1):
+            n1 = n1 * -1
+            result = n1**(1/n2)
+            result = result * -1
+     else:
+         result = n1 ** (1/n2)
+    return result
+
+def calcula_multiplicação(n1, n2):
+	return n1 * n2
+
+def calcula_divisão(n1, n2):
+	return n1 / n2
+
 def calcula_soma(x,y):
     return x+y
 
@@ -6,7 +24,7 @@ def calcula_porcentagem(x,y):  #Quero calcular x porcento de y"
 
 def escolhaDeOperação():
     while True:
-        operador= input("Escolha uma das operações:\n adição \n subtração \n multiplicação \n divisão\n exponenciação \n raidiciação\n divisão inteira\n porcentagem\n ou\n digite 0 para encerrar:").lower()
+        operador= input("Escolha uma das operações:\n adição \n subtração \n multiplicação \n divisão\n exponenciação \n raidiciação\n divisão inteira\n porcentagem\n resto\n ou\n digite 0 para encerrar:").lower()
         if operador == "0":
             print("Operação encerrada")
             break
@@ -34,6 +52,9 @@ def escolhaDeOperação():
             print("----------------")
             
         elif operador == "divisão":
+            if n2==0:
+                print("não existe divisão por 0)
+                return
             print("----------------")
             print("\n")
             print(calcula_divisão(n1,n2))
@@ -48,13 +69,23 @@ def escolhaDeOperação():
             print("----------------")
             
         elif operador == "radiciação":
+            if n2==0:
+                print("não existe raiz de indice 0)
+                      return
+            elif n1<0 and n2%2 == 0:
+                print("Não existe raiz paz de numero negativo)
+                return
+                
             print("----------------")
             print("\n")
-            print(calcula_radiciação(n1,n2))
+            print(ccalcula_radiciação(n1,n2))
             print("\n")
             print("----------------")
             
         elif operador == "divisao inteira":
+            if n2==0:
+            print("Não existe divisão por 0)
+            return
             print("----------------")
             print("\n")
             print(calcula_divisão_inteira(n1,n2))
@@ -67,13 +98,17 @@ def escolhaDeOperação():
             print(calcula_porcentagem(n1,n2))
             print("\n")
             print("----------------")
+
+        elif operador == "resto":
+        if n2== 0:
+            print("Não existe resto de divisão por 0")
+            return
+        print("----------------")
+            print("\n")
+            print(calcula_resto(n1,n2))
+            print("\n")
+            print("----------------")
           
 
 escolhaDeOperação()
-
-def calcula_resto(n1,n2):
-    return n1 % n2
-
-def calcula_radiciação(n1, n2):
-    return n1 ** (1 / n2)
 
